48acbfcdbbc6746988515a7f25470d43f2d566b6
Aleksei Ozhiganov <aleksei.ozhiganov@gelato.com>
2023-04-09 18:11:41 +0600

    first version

--- src/cron.ts
+++ src/cron.ts
@@ -7,9 +7,9 @@ let lastRun = 0;
 export function runCron() {
   schedule(`10 * * * *`, async () => {
     if (lastRun !== +moment().format('HH')) {
+      lastRun = +moment().format('HH');
       console.log(`Cron job run at ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
       run();
-      lastRun = +moment().format('HH');
     }
   });
 }
